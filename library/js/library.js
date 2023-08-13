9374c041e3a748c00fd147b620600f8f2f6942b5
ulja4a <ulja4a@gmail.com>
2023-08-13 18:01:34 +0400

    feat: add burger menu open and close

---
+++ library/js/library.js
@@ -0,0 +1,26 @@
+(function () {
+  const burger = document.querySelector('.header__burger');
+  const menu = document.querySelector('.nav');
+  const menuClose = document.querySelector('.nav__close');
+  const menuItem = document.querySelectorAll('.nav-link');
+  const menuIcon = document.querySelector('.icon__burger');
+
+  
+
+  burger.addEventListener('click', (e) => {
+    menu.classList.add('nav_active');
+  });
+  menuClose.addEventListener('click', (e) => {
+    menu.classList.remove('nav_active');
+  });
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.remove('nav_active');
+    });
+  });
+  document.onclick = function(event) {
+    if (!menu.contains(event.target) && event.target !== burger && !menu.contains(event.target) && !menuClose.contains(event.target) && event.target !== menuIcon) {
+      menu.classList.remove('nav_active');
+    }
+  }
+} ());
