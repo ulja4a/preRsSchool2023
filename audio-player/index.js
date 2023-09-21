1af3989863b5266a415d2867a0b13b107c7096e0
ulja4a <ulja4a@gmail.com>
2023-09-22 00:21:17 +0400

    feat: add work button play and pause for one song

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,7 +1,29 @@
+const audioPlay = document.querySelector('.play-pause');
+
 function changeBackground() {
   document.body.style.backgroundImage = 'url("./assets/img/earth_wind_fire_boogie_wonderland.jpg")';
 }
 changeBackground();
+let audio = new Audio();
+let isPlay = false;
 
+function playAudio() {
+  if (!isPlay) {
+    audio.src = 'http://127.0.0.1:5500/audio-player/assets/audio/Earth_Wind_Fire_-_Boogie_Wonderland_48181877.mp3';
+    audio.currentTime = 0;
+    audio.play();
+    audioPlay.classList.remove('play-btn');
+    audioPlay.classList.add('pause-btn');
+  } else {
+    audio.pause();
+    audioPlay.classList.remove('pause-btn');
+    audioPlay.classList.add('play-btn');
+  }
+  isPlay = !isPlay;
+}
+
+audioPlay.addEventListener('click', () => {
+  playAudio();
+});
 
 
