6b82e53ef227ac801b4b29607a2979773e3865c1
ulja4a <ulja4a@gmail.com>
2023-09-22 11:20:30 +0400

    feat: add work prev and next button

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,15 +1,36 @@
 const audioPlay = document.querySelector('.play-pause');
+const buttonNext = document.querySelector('.play-next');
+const buttonPrev = document.querySelector('.play-prev');
+
+const playList = [
+  {
+    url: 'http://127.0.0.1:5500/audio-player/assets/audio/Earth_Wind_Fire_-_Boogie_Wonderland_48181877.mp3',
+    name: 'Boogie Wonderland',
+    autor: 'Earth, Wind & Fire',
+    cover: 'http://127.0.0.1:5500/audio-player/assets/img/earth_wind_fire_boogie_wonderland.jpg'
+  },
+  {
+    url: 'http://127.0.0.1:5500/audio-player/assets/audio/behappy.mp3',
+    name: "Don't Worry Be Happy",
+    autor: 'Bobby McFerrin',
+    cover: 'http://127.0.0.1:5500/audio-player/assets/img/behappy.jpg'
+  }
+]
 
 function changeBackground() {
   document.body.style.backgroundImage = 'url("./assets/img/earth_wind_fire_boogie_wonderland.jpg")';
 }
 changeBackground();
+
 let audio = new Audio();
 let isPlay = false;
+let playNum = 0;
+
+
 
 function playAudio() {
   if (!isPlay) {
-    audio.src = 'http://127.0.0.1:5500/audio-player/assets/audio/Earth_Wind_Fire_-_Boogie_Wonderland_48181877.mp3';
+    audio.src = playList[playNum].url;
     audio.currentTime = 0;
     audio.play();
     audioPlay.classList.remove('play-btn');
@@ -26,4 +47,35 @@ audioPlay.addEventListener('click', () => {
   playAudio();
 });
 
+function playNext() {
+  if (playNum < playList.length - 1) {
+    playNum++;
+  } else {
+    playNum = 0;
+  }
+  if (isPlay) {
+    playAudio();
+  }
+  console.log(playNum);
+}
+
+buttonNext.addEventListener('click', () => {
+  playNext();
+})
+
+function playPrev() {
+  if (playNum > 0) {
+    playNum--;
+  } else {
+    playNum = playList.length - 1;
+  }
+  if (isPlay) {
+    playAudio();
+  }
+  console.log(playNum);
+}
+
+buttonPrev.addEventListener('click', () => {
+  playPrev();
+})
 
