f6663775018e3627bfdc891a4eb70c2381a06e16
ulja4a <ulja4a@gmail.com>
2023-09-26 20:17:02 +0400

    feat: add function serch at click serch icon and sort pictures

--- image-galery/index.js
+++ image-galery/index.js
@@ -1,12 +1,16 @@
 const gallery = document.querySelector('.pictures');
+const input = document.getElementById('input');
+const search = document.querySelector('.search-icon');
 
-url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=4e94dea62bb855253f145724d952eac2&tags=spring,nature&tag_mode=all&extras=url_m&format=json&nojsoncallback=1`
+/*url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=4e94dea62bb855253f145724d952eac2&tags=nature&tag_mode=all&extras=url_m&format=json&nojsoncallback=1`*/
+url = `https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=30&api_key=4e94dea62bb855253f145724d952eac2&extras=url_m&format=json&nojsoncallback=1`;
 
 async function getData() {
-  const pictures = await fetch(url);
-  const data = await pictures.json();
+  let pictures = await fetch(url);
+  let data = await pictures.json();
   console.log(data);
-  const photos = data.photos.photo;
+  gallery.innerHTML = '';
+  let photos = data.photos.photo;
     photos.forEach(photo => {
       let imgSrc = photo.url_m;
       let div = document.createElement('div');
@@ -15,10 +19,20 @@ async function getData() {
       gallery.appendChild(div);
     });
 }
+  function performSearch() {
+      let searchValue = input.value;
+      console.log(searchValue)
+      url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&per_page=30&api_key=4e94dea62bb855253f145724d952eac2&tags=${input.value}&tag_mode=all&extras=url_m&format=json&nojsoncallback=1`;
+      getData();
+  }
+    
+  search.addEventListener('click', performSearch);
+
+  document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("input").focus();
+  });
+
 getData();
 
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("input").focus();
-});
 
 
